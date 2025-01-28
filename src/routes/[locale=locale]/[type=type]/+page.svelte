f1cf3acd7c726944f6474bb5e17aa3728ccda06e
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-01-28 21:26:46 +0100

    add type params

---
+++ src/routes/[locale=locale]/[type=type]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import { RouteType } from "$enums";
+  import { locale, t } from "$lib/translations";
+</script>
+
+<svelte:head>
+  <title>{$t(`route.type.${RouteType.Contact}.title`)}</title>
+  <meta name="description" content="" />
+</svelte:head>
+
+<div class="text-center text-column">
+  {$locale}
+  <br>
+  {$t('route.type.contact.title')}
+</div>
