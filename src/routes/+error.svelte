80169667ce6f5b2e086f977452d5db6276ca16e4
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-01-28 21:28:12 +0100

    create error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { defaultLocale, locale } from "$lib/translations";
+
+  const url: string = $locale ? `/${$locale}` : `/${defaultLocale}`;
+</script>
+
+<section
+        class="h-screen w-screen overflow-hidden bg-gray-950 text-slate-600 flex items-center justify-center relative z-0">
+    <div>
+        <p>&mdash; <span class="text-amber-800">404</span></p>
+        <h1 class="text-2xl">Votre page est perdue quelque part dans le web</h1>
+        <a href={url} class="text-blue-500">
+            <!-- //FIXME translation -->
+             Retour à l'accueil
+        </a>
+    </div>
+</section>
