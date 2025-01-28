c2fb9c951f307c57c2717179255aa970021280b1
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-01-28 13:27:08 +0100

    add types to test

---
+++ src/params/type.ts
@@ -0,0 +1,114 @@
+// import type { Locale } from "$lib/translations";
+
+
+// const routes: TypeRoute[] = [
+//     {
+//         name: RoutesSlugs.Home,
+//         title: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//         slug: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//     },
+//     {
+//         name: RoutesSlugs.Contact,
+//         title: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//         slug: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//     },
+//     {
+//         name: RoutesSlugs.Articles,
+//         title: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//         slug: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//     },
+//     {
+//         name: RoutesSlugs.Themes,
+//         title: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//         slug: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//     },
+//     {
+//         name: RoutesSlugs.Coverage,
+//         title: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//         slug: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//     },
+//     {
+//         name: RoutesSlugs.Pressrelease,
+//         title: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//         slug: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//     },
+//     {
+//         name: RoutesSlugs.Presskit,
+//         title: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//         slug: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//     },
+//     {
+//         name: RoutesSlugs.Highlights,
+//         title: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//         slug: {
+//             fr: '',
+//             en: '',
+//             de: '',
+//         },
+//     }
+// ]
+
+
+export const match = (param: string): boolean => {
+    return true;
+}
