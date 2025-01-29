ef5494fc2dd28e89f1653e94bf175ce29ea04725
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-01-29 11:32:11 +0100

    create Themes enum

--- src/lib/enums/index.ts
+++ src/lib/enums/index.ts
@@ -1,12 +1,27 @@
 export type RouteType = typeof RouteTypes[keyof typeof RouteTypes]
+export type Theme = typeof Themes[keyof typeof Themes]
 
 export const RouteTypes = {
   Home: "home",
-  Contact: "contact",
   Articles: "articles",
-  Themes: 'themes',
+  Contact: "contact",
   Coverage: "coverage",
+  Highlights: "highlights",
+  Themes: 'themes',
   Pressrelease: "press-release",
   Presskit: "press-kit",
-  Highlights: "highlights",
+} as const;
+
+export const Themes = {
+  Nature: 'nature',
+  Education: 'education',
+  Culture: 'culture',
+  Gastronomy: 'gastronomy',
+  Sustainability: 'sustainability',
+  Family: 'family',
+  Architecture: 'architecture',
+  Lacustrine: 'lacustrine',
+  Wellness: 'wellness',
+  Unusual: 'unusual',
+  Sport: 'sport',
 } as const;
