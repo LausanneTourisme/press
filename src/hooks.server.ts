786995092e74696d387c641646ae25eb713aa137
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-01-30 17:17:54 +0100

    creates hooks.server.ts

---
+++ src/hooks.server.ts
@@ -0,0 +1,44 @@
+import {error, redirect, type Handle, type ResolveOptions} from '@sveltejs/kit';
+import {sequence} from "@sveltejs/kit/hooks";
+import {get} from "svelte/store";
+import { defaultLocale, isValidLocale, setLocale, type Locale } from '$lib/translations';
+import type { RequestEvent } from '@sveltejs/kit';
+
+const getNavLocale = (request: Request): Locale => {
+    // If no cookie is set, try to determine the locale from the 'Accept-Language' header
+    const acceptLanguageHeader = request.headers.get('accept-language') || '';
+    // Attempt to match the language code with optional region code
+    let match = acceptLanguageHeader.match(/^[a-z]+(?=[-_])/i);
+
+    // If no match is found, try to match just the language code
+    if (!match) {
+        match = acceptLanguageHeader.match(/^[a-z]+/i);
+    }
+
+    if (match && isValidLocale(match[0])) return match[0].toLowerCase() as Locale;
+
+    // If a match is found, use it as the locale, otherwise fall back to the default locale
+    return defaultLocale;
+}
+
+const resolveLanguage = async ({event, resolve}) => {
+    if (event.url.pathname === '/') {
+        redirect(302, `/${getNavLocale(event.request)}`)
+    }
+
+    if(!isValidLocale(event.params.locale ?? '')){
+        await setLocale(defaultLocale);
+        return await resolve(event, {
+            transformPageChunk: ({html}) => html.replace('%lang%', defaultLocale)
+        });
+    }
+
+    const locale = event.params.locale as Locale|undefined  ?? defaultLocale;
+    await setLocale(locale);
+    
+    return await resolve(event, {
+        transformPageChunk: ({html}) => html.replace('%lang%', locale)
+    });
+}
+
+export const handle: Handle = sequence(resolveLanguage)
