7ded3cb84cc4155299974f9ea584a8b9c288ef46
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-01-30 11:46:23 +0100

    create SeoHeader type

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,12 +1,26 @@
+import type { Locale } from "$lib/translations";
 import type { SvelteComponent } from "svelte";
 
 export type MenuItem = {
     title: string,
     link?: string,
     anchor?: string,
-    icon?: SvelteComponent,
+    icon?: SvelteComponent
 }
 
 export type Menu = MenuItem & {
     items?: MenuItem[]
+}
+
+export type SeoHeader = {
+    canonical: string,
+    title: string,
+    description: string,
+    image: string,
+    alternate: SeoAlternate[]
+}
+
+export type SeoAlternate = {
+    hreflang: Locale,
+    href: string
 }
