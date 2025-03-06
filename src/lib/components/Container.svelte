9d0dab1c12fe8e3c548354709d196183bc04f837
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-03-06 10:00:01 +0100

    create container

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import { twMerge } from 'tailwind-merge';
+
+  type ContainerProps = {
+    fullscreen?: boolean;
+    width?: 'padded' | 'large' | 'medium' | 'small' | 'agenda';
+    sticky?: boolean;
+    background?: string;
+    class?: string;
+    children: Snippet;
+  };
+
+  const {
+    fullscreen = false,
+    width = 'large',
+    sticky = false,
+    background = '',
+    class: additionalClass,
+    children
+  }: ContainerProps = $props();
+
+  function getWidthStyle(width: string) {
+    switch (width) {
+      default:
+      case 'padded':
+        return 'md:p-16';
+      case 'large':
+        return 'md:max-w-[1536px] mx-auto';
+      case 'medium':
+        return 'md:max-w-[1024px] mx-auto';
+      case 'small':
+        return 'md:max-w-[720px] mx-auto';
+      case 'agenda':
+        return 'md:px-16';
+    }
+  }
+
+  const style = twMerge(
+    !fullscreen ? `p-6 ${getWidthStyle(width)}` : 'overflow-hidden max-w-screen',
+    sticky ? 'sticky top-24' : '',
+    additionalClass
+  );
+</script>
+
+<!-- wrapping div to help with background colors and other...-->
+<div class="w-full {background}">
+  <section class={style}>
+    {@render children()}
+  </section>
+</div>
