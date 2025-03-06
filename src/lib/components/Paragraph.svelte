c3dbaec80c4810463c8b9c2e8a806022951601b8
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-03-06 21:01:27 +0100

    create paragraph component

---
+++ src/lib/components/Paragraph.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+  import { twMerge } from 'tailwind-merge';
+
+  type HeadingProps = {
+    class?: string;
+    centered?: boolean;
+    children: Snippet;
+  };
+
+  let { class: additionalClass = '', centered = false, children }: HeadingProps = $props();
+
+  const style = twMerge(
+    'text-neutral-700 font-lighter leading-7 my-3',
+    centered ? 'md:text-center mx-auto max-w-[560px] md:max-w-[800px]' : '',
+    additionalClass
+  );
+</script>
+
+<p class={style}>
+  {@render children()}
+</p>
