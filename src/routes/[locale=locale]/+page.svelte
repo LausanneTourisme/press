e7a3cf6a8bb45a803c50ad24a3f4d444828a79c4
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-03-11 08:21:31 +0100

    update home page

--- src/routes/[locale=locale]/+page.svelte
+++ src/routes/[locale=locale]/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { browser } from '$app/environment';
   import { RouteTypes } from '$enums';
   import Button from '$lib/components/Button.svelte';
   import Container from '$lib/components/Container.svelte';
@@ -7,33 +6,54 @@
   import Image from '$lib/components/Media/Image.svelte';
   import Player from '$lib/components/Media/Player.svelte';
   import Paragraph from '$lib/components/Paragraph.svelte';
-  import { route } from '$lib/helpers';
-  import { locale, t, translations } from '$lib/translations';
-  import Device from 'svelte-device-info';
-  
+  import Themes from '$lib/components/Themes.svelte';
+  import { getMediaLibraryRegisterLink, route, isDarkMode as getDarkMode } from '$lib/helpers';
+  import { locale, t, type Locale } from '$lib/translations';
+  import { onMount } from 'svelte';
+
+  let isDarkMode = $state(false);
+
+  onMount(() => {
+    const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Set initial value
+    isDarkMode = getDarkMode();
+
+    // Listen for changes
+    const updateDarkMode = () => {
+      isDarkMode = getDarkMode();
+    };
+
+    darkModeQuery.addEventListener('change', updateDarkMode);
+
+    // Cleanup event listener when the component is destroyed
+    return () => darkModeQuery.removeEventListener('change', updateDarkMode);
+  });
+
   const onVideoIntersecting = () => {
-        const nav = document.getElementById('main-nav');
-        if (nav) {
-            if (browser && !Device.isMobile) {
-                nav.classList.remove('bg-white', 'shadow-lg');
-                nav.classList.add('invert-colours', 'bg-transparent', 'text-white');
-            } else {
-                nav.classList.remove('invert-colours', 'bg-transparent', 'text-white');
-                nav.classList.add('bg-white', 'shadow-lg');
-            }
-        }
+    console.log("intersecting video")
+  //       const nav = document.getElementById('main-nav');
+  //       if (nav) {
+  //           if (browser && !Device.isMobile) {
+  //               nav.classList.remove('bg-white', 'shadow-lg');
+  //               nav.classList.add('invert-colours', 'bg-transparent', 'text-white');
+  //           } else {
+  //               nav.classList.remove('invert-colours', 'bg-transparent', 'text-white');
+  //               nav.classList.add('bg-white', 'shadow-lg');
+  //           }
+  //       }
     };
 </script>
 
 <!--
--
--
--
-- HERO >>>>>>>>>>>>>>>>>>>>>>>
--
--
--
--->
+  -
+  -
+  -
+  - HERO >>>>>>>>>>>>>>>>>>>>>>>
+  -
+  -
+  -
+  -->
 <Container
   fullscreen={true}
   class="relative min-h-[500px] items-end justify-start overflow-hidden md:flex md:items-center"
@@ -83,11 +103,132 @@
   -->
 <Container class="relative z-10 py-[3vh]" fullscreen={true}>
   <!-- FIXME <3 -->
-  <Player
+  <!-- <Player
     src="/videos/welcome_card_{$locale}.mp4"
     poster="/images/pages/home/poster-video.png"
     controls={true}
-    onIntersecting={() => onVideoIntersecting()}
+    onIntersecting={onVideoIntersecting}
     title="Welcome to Lausanne!"
-  />
+  /> -->
+  <div>I don't want see video everytime</div>
 </Container>
+<!--
+  -
+  -
+  -
+  - VIDEOS AND IMAGES >>>>>>>>>>>>>>>>>>>>>>>
+  -
+  -
+  -
+  -->
+<Container fullscreen={false}>
+  <div class="md:flex md:h-96 md:flex-row-reverse">
+    <article
+      class="bg-shakespeare-100 dark:bg-shakespeare-400 flex flex-col justify-center p-6 py-12 md:w-1/2 md:p-16"
+    >
+      <Heading class="dark:text-white">
+        {$t('page.video&images.title')}
+      </Heading>
+      <Paragraph class="dark:text-white">
+        {$t('page.video&images.paragraph')}
+      </Paragraph>
+      <div>
+        {#key isDarkMode}
+          <Button negative={isDarkMode} href={getMediaLibraryRegisterLink($locale as Locale)}>
+            {$t('page.goToMediaLibrary')}
+          </Button>
+        {/key}
+      </div>
+    </article>
+    <Image
+      src="/images/pages/home/masonry/cathedrale.jpg"
+      transform={{ g: 'auto', c: 'fill', ar: '9:16' }}
+      alt="Cathédrale de Lausanne"
+      class="h-72 md:h-auto md:w-1/2"
+    />
+  </div>
+</Container>
+<!--
+  -
+  -
+  -
+  - Ressources >>>>>>>>>>>>>>>>>>>>>>>
+  -
+  -
+  -
+  -->
+<Container fullscreen={true}>
+  <div class="mt-6 md:flex md:h-96">
+    <article
+      class="bg-shakespeare-400 flex flex-col justify-center p-6 py-12 text-white md:w-1/2 md:p-16"
+    >
+      <Heading class="text-white">
+        {$t('page.whatIsNew.title')}
+      </Heading>
+      <Paragraph class="text-white">
+        {$t('page.whatIsNew.paragraph')}
+      </Paragraph>
+      <div>
+        <Button negative={true} href={route(RouteTypes.Highlights)}>
+          {$t('common.btn.learnMore')}
+        </Button>
+      </div>
+    </article>
+    <Image
+      src="/images/pages/home/nouveautes-lacustre.jpg"
+      alt=""
+      class="h-72 md:h-auto md:w-1/2"
+    />
+  </div>
+  <div class="md:flex md:h-96 md:flex-row-reverse">
+    <article
+      class="bg-shakespeare-400 flex flex-col justify-center p-6 py-12 text-white md:w-1/2 md:p-16"
+    >
+      <Heading class="text-white">
+        {$t('page.pressRelease.title')}
+      </Heading>
+      <Paragraph class="text-white">
+        {$t('page.pressRelease.paragraph')}
+      </Paragraph>
+      <div>
+        <Button negative={true} href={route(RouteTypes.Pressrelease)}>
+          {$t('common.btn.learnMore')}
+        </Button>
+      </div>
+    </article>
+    <Image
+      src="/images/pages/home/communiques-de-presse.jpg"
+      transform={{ c: 'fill' }}
+      alt=""
+      class="h-72 md:h-auto md:w-1/2"
+    />
+  </div>
+  <div class="md:flex md:h-96">
+    <article
+      class="bg-shakespeare-400 flex flex-col justify-center p-6 py-12 text-white md:w-1/2 md:p-16"
+    >
+      <Heading class="text-white">
+        {$t('page.mediaCoverage.title')}
+      </Heading>
+      <Paragraph class="text-white">
+        {$t('page.mediaCoverage.paragraph')}
+      </Paragraph>
+      <div>
+        <Button negative={true} href={route(RouteTypes.Coverage)}>
+          {$t('common.btn.learnMore')}
+        </Button>
+      </div>
+    </article>
+    <Image src="/images/pages/home/exposition.jpg" alt="" class="h-72 md:h-auto md:w-1/2" />
+  </div>
+</Container>
+<!--
+  -
+  -
+  -
+  - THEMES >>>>>>>>>>>>>>>>>>>>>>>
+  -
+  -
+  -
+  -->
+  <Themes/>
