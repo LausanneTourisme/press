aedb442c648e4ba41f48e52e77b4d1eb4f638352
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-03-11 08:50:35 +0100

    creates Anchor

---
+++ src/lib/components/Anchor.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { twMerge } from 'tailwind-merge';
+
+    type Props = {
+      anchor?: string;
+      class?: string;
+    };
+
+    const {  class: additionalClass, anchor }: Props = $props();
+    const style = twMerge(
+      "relative -top-14 md:-top-32",
+      additionalClass
+    );
+  </script>
+
+<div class={style}>
+    <span id={anchor} class="absolute -z-50"></span>
+</div>
+
+<style>
+    /**
+     * Because the navbar is static, we want to offset its height to this span so it aligns perfectly.
+    */
+    span {
+        height:0;
+        width:0;
+    }
+</style>
