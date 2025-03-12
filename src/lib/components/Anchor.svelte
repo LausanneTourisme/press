f455c9058e962640b0885ff232d32936dc7409d0
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-03-12 08:02:53 +0100

    refactor Anchor

--- src/lib/components/Anchor.svelte
+++ src/lib/components/Anchor.svelte
@@ -1,28 +1,25 @@
 <script lang="ts">
-    import { twMerge } from 'tailwind-merge';
+  import { twMerge } from 'tailwind-merge';
 
-    type Props = {
-      anchor?: string;
-      class?: string;
-    };
+  type Props = {
+    name?: string;
+    class?: string;
+  };
 
-    const {  class: additionalClass, anchor }: Props = $props();
-    const style = twMerge(
-      "relative -top-14 md:-top-32",
-      additionalClass
-    );
-  </script>
+  const { class: additionalClass, name }: Props = $props();
+  const style = twMerge('relative -top-14 md:-top-32', additionalClass);
+</script>
 
 <div class={style}>
-    <span id={anchor} class="absolute -z-50"></span>
+  <span id={name} class="absolute -z-50"></span>
 </div>
 
 <style>
-    /**
+  /**
      * Because the navbar is static, we want to offset its height to this span so it aligns perfectly.
     */
-    span {
-        height:0;
-        width:0;
-    }
+  span {
+    height: 0;
+    width: 0;
+  }
 </style>
