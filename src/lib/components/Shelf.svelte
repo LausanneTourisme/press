27d0b69dfcb024c21d64406d62d09716ba79243f
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-03-12 14:13:44 +0100

    Shelf tilte could be Snippet or text

--- src/lib/components/Shelf.svelte
+++ src/lib/components/Shelf.svelte
@@ -1,39 +1,43 @@
 <script lang="ts">
-    import { ChevronDown } from 'lucide-svelte';
-    import type { Snippet } from 'svelte';
-    import { twMerge } from 'tailwind-merge';
-    import { slide } from 'svelte/transition';
+  import { ChevronDown } from 'lucide-svelte';
+  import type { Snippet } from 'svelte';
+  import { twMerge } from 'tailwind-merge';
+  import { slide } from 'svelte/transition';
 
-    type Props = {
-      class?: string;
-      titleClass?: string;
-      isOpen: boolean;
-      onToggle: () => void;
-      children: Snippet;
-      title: string;
-    };
+  type Props = {
+    class?: string;
+    titleClass?: string;
+    isOpen: boolean;
+    onToggle: () => void;
+    children: Snippet;
+    title: string | Snippet;
+  };
 
-    const { class: additionalClass, children, title, titleClass, isOpen, onToggle }: Props = $props();
-    const style = twMerge(additionalClass);
-  </script>
+  const { class: additionalClass, children, title, titleClass, isOpen, onToggle }: Props = $props();
+  const style = twMerge(additionalClass);
+</script>
 
-  <div class={style}>
-    <button
-      class={twMerge("cursor-pointer font-semibold flex justify-between pr-1 w-full", titleClass)}
-      onclick={onToggle}
-    >
-      {title}
-      <span
-        class="inline-flex items-center justify-center transition-transform duration-300"
-        class:rotate-180={isOpen}
-      >
-        <ChevronDown strokeWidth={3} class="inline h-4 w-4" />
-      </span>
-    </button>
-
-    {#if isOpen}
-      <div transition:slide class="mt-2 space-y-2">
-        {@render children()}
-      </div>
+<div class={style}>
+  <button
+    class={twMerge('flex w-full cursor-pointer justify-between pr-1 font-semibold', titleClass)}
+    onclick={onToggle}
+  >
+    {#if typeof title === 'function'}
+      {@render title()}
+    {:else}
+      <span>{title}</span>
     {/if}
-  </div>
+    <span
+      class="inline-flex items-center justify-center transition-transform duration-300"
+      class:rotate-180={isOpen}
+    >
+      <ChevronDown strokeWidth={3} class="inline h-4 w-4" />
+    </span>
+  </button>
+
+  {#if isOpen}
+    <div transition:slide class="space-y-2">
+      {@render children()}
+    </div>
+  {/if}
+</div>
