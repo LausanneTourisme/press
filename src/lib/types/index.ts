6767ae68b14b1563743e6c6ebfa3698d07206b1e
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-04-09 13:28:52 +0200

    add new types

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -26,3 +26,50 @@ export type SeoAlternate = {
     hreflang: Locale,
     href: string
 }
+
+export type Translatable = {
+    fr: string,
+    en?: string,
+    de?: string,
+    it?: string,
+    es?: string,
+}
+
+export type Media = {
+    id: number,
+    is_cover?: boolean,
+    name: string,
+    public_name: Translatable,
+    cover: boolean,
+    cloudinary_id: string,
+    copyright: string,
+    metadata: object,
+}
+
+export type Tag = {
+    id: number,
+    name: string,
+    public_name: string,
+}
+
+export type Seo = {
+    id: number,
+    name: string,
+    slug: string,
+    hreflang: string,
+}
+
+export type Release = {
+    published_at: string,
+    id: number,
+    name: Translatable,
+    lead: Translatable,
+    summary: Translatable,
+    medias: Media[],
+    type: string,
+    seo: Seo,
+    tags: Tag[],
+    highlight: boolean,
+}
+export type Post = Release;
+export type Page = Release;
