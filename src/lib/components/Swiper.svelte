184b9880ca2271179b3de42e9e09dab5d3ab1c99
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-04-09 13:28:42 +0200

    fix swipper class warnings

--- src/lib/components/Swiper.svelte
+++ src/lib/components/Swiper.svelte
@@ -83,6 +83,7 @@
     };
 
     emblaOptions = { ...defaultOptions, ...options };
+    canScroll();
     /**
      * EVENT LISTENERS
      */
@@ -114,14 +115,14 @@
   <div class:hidden={!showButtons}>
     <button
       bind:this={previousButton}
-      class="embla__prev absolute top-[calc(50%_-_48px)] left-1 flex hidden h-16 w-16 items-center justify-center rounded-full bg-red-600/60 text-white transition-[background] hover:bg-red-600"
+      class="embla__prev absolute top-[calc(50%_-_48px)] left-1 flex h-16 w-16 items-center justify-center rounded-full bg-red-600/60 text-white transition-[background] hover:bg-red-600"
       onclick={prev}
     >
       <ChevronLeft class="h-4 w-4" />
     </button>
     <button
       bind:this={nextButton}
-      class="embla__next absolute top-[calc(50%_-_48px)] right-1 flex hidden h-16 w-16 items-center justify-center rounded-full bg-red-600/60 text-white transition-[background] hover:bg-red-600"
+      class="embla__next absolute top-[calc(50%_-_48px)] right-1 flex h-16 w-16 items-center justify-center rounded-full bg-red-600/60 text-white transition-[background] hover:bg-red-600"
       onclick={next}
     >
       <ChevronRight class="h-4 w-4" />
