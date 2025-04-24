703a58cd591be6b09bb52d427c31a1ee4afc7aee
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-04-24 17:32:10 +0200

    fix hot reload mockups

--- src/lib/mocks/handler.ts
+++ src/lib/mocks/handler.ts
@@ -1,19 +1,57 @@
 import { graphql, HttpResponse, } from 'msw';
 import { setupServer } from 'msw/node';
 
+import eventsMock from './responses/events/all.json'
+const favoritesMocks = import.meta.glob('./responses/favorites/*.json', { eager: true });
+const postsMocks = import.meta.glob('./responses/posts/*.json', { eager: true });
+
+const favoritesMap: Record<string, any> = {};
+const postsMap: Record<string, any> = {};
+
+for (const path in favoritesMocks) {
+	const filename = path.split('/').pop()?.replace('.json', ''); // ex: culture.fr
+	if (filename) {
+		favoritesMap[filename] = (favoritesMocks[path] as any).default;
+	}
+}
+
+for (const path in postsMocks) {
+	const filename = path.split('/').pop()?.replace('.json', ''); // ex: news.fr
+	if (filename) {
+		postsMap[filename] = (postsMocks[path] as any).default;
+	}
+}
+
 export const handlers = [
     graphql.query('GetPosts', async ({ variables }) => {
-        console.warn("mock request: GetPosts");
+        
         if (variables.type === 'press_release, press_kit') {
-            return HttpResponse.json(await import(`./responses/posts/press_kit.${variables.locale}.json`));
+            console.warn("mock request: GetPosts (press)");
+            const key = `press_kit.${variables.locale}`;
+            const mock = postsMap[key];
+
+            return HttpResponse.json(mock);
         } else if (variables.type === "post") {
-            if (variables.highlighted) {
-                return HttpResponse.json(await import(`./responses/posts/posts.highlighted.${variables.locale}.json`));
+            if(variables.highlighted){
+                console.warn("mock request: GetPosts (highlighted posts)");
+                const key = `posts.highlighted.${variables.locale}`;
+                const mock = postsMap[key];
+                
+                return HttpResponse.json(mock);
             }
-            return HttpResponse.json(await import(`./responses/posts/posts.${variables.locale}.json`));
+            console.warn("mock request: GetPosts (posts)");
+            const key = `posts.${variables.locale}`;
+            const mock = postsMap[key];
+
+            return HttpResponse.json(mock);
         } else if (variables.type === "news") {
-            return HttpResponse.json(await import(`./responses/posts/news.${variables.locale}.json`));
+            console.warn("mock request: GetPosts (news)");
+            const key = `news.${variables.locale}`;
+            const mock = postsMap[key];
+
+            return HttpResponse.json(mock);
         } else {
+            console.error("mock request: GetPosts (no idea...)");
             return HttpResponse.json({
                 "data": {
                     "items": {
@@ -30,15 +68,20 @@ export const handlers = [
     }),
     graphql.query('GetGroup', async ({ variables }) => {
         console.warn("mock request: GetGroup");
-        return HttpResponse.json(await import(`./responses/groups/posts.${variables.locale}.json`));
+        // TODO
+        // return HttpResponse.json(await import(`./responses/groups/posts.${variables.locale}.json`));
+        return HttpResponse.json({})
     }),
-    graphql.query('GetFavorites', async ({ variables }) => {
+    graphql.query('GetFavorites',async ({ variables }) => {
         console.warn("mock request: GetFavorites");
-        return HttpResponse.json(await import(`./responses/favorites/${variables.theme}.${variables.locale}.json`));
+		const key = `${variables.theme}.${variables.locale}`; // ex: "culture.fr"
+		const mock = favoritesMap[key];
+
+        return HttpResponse.json(mock);
     }),
-    graphql.query('GetAgendaEvents', async () => {
+    graphql.query('GetAgendaEvents',async () => {
         console.warn("mock request: GetAgendaEvents");
-        return HttpResponse.json(await import(`./responses/events/all.json`));
+        return HttpResponse.json(eventsMock);
     }),
 ];
 
