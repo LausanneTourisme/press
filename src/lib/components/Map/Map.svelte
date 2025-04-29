37a214c50dfc32d13d40c5d17b2f02015d906b16
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-04-29 19:20:38 +0200

    refactor map

--- src/lib/components/Map/Map.svelte
+++ src/lib/components/Map/Map.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-  import { PUBLIC_CLOUDINARY_UPLOAD_PRESET, PUBLIC_MAPTILER_URL } from '$env/static/public';
-  import { Cloudinary } from '$lib/cloudinary';
-  import { getBoundsFromMarkers } from '$lib/coordinates';
-  import { isOfflineMode } from '$lib/helpers';
+  import { PUBLIC_MAPTILER_URL } from '$env/static/public';
+  import { getTailwindColor, isOfflineMode } from '$lib/helpers';
   import { defaultLocale, locale, t, type Locale } from '$lib/translations';
-  import type { Favorite, Geolocation, Lausanner, Marker, Poi } from '$types';
-  import { ArrowRight, SquareArrowOutUpRight, X } from 'lucide-svelte';
-  import maplibregl from 'maplibre-gl';
+  import type { Coordinate, Favorite, Geolocation, Lausanner, Poi } from '$types';
+  import { ArrowRight, MapPin, SquareArrowOutUpRight, X } from 'lucide-svelte';
   import { onMount } from 'svelte';
+  import { MapLibre, Marker, NavigationControl, Popup } from 'svelte-maplibre-gl';
   import { fade } from 'svelte/transition';
   import { twMerge } from 'tailwind-merge';
   import Heading from '../Heading.svelte';
@@ -15,7 +13,6 @@
   import Image from '../Media/Image.svelte';
   import Paragraph from '../Paragraph.svelte';
   import LausannerCard from './LausannerCard.svelte';
-  import MarkerComponent from './Marker.svelte';
 
   type Props = {
     class?: string;
@@ -33,11 +30,15 @@
     onclose
   }: Props = $props();
 
-  const { Map, NavigationControl, Popup } = maplibregl;
+  const markers: {
+    poiName: string;
+    lausanner?: Lausanner;
+    favorite: Favorite;
+    coordinates: Coordinate;
+  }[] = $state([]);
   let favoriteId: number | undefined = $state();
-  let mapContainer: HTMLDivElement;
-  const markers: Marker[] = [];
-  let map: maplibregl.Map|undefined = $state();
+
+  const initialState = { lat: 46.5197163, lng: 6.6309901, zoom: 13 };
 
   let aside: {
     show: boolean;
@@ -57,20 +58,32 @@
     lausanner: undefined
   });
 
-  const initialState = {
-    lat: 46.5197163,
-    lng: 6.6309901,
-    zoom: 13
+  const getLausannerUrl = ({
+    lausanner,
+    locale
+  }: {
+    lausanner: Lausanner | undefined;
+    locale: Locale;
+  }): string => {
+    if (!lausanner) return '#';
+
+    return (
+      {
+        fr: 'https://www.lausanne-tourisme.ch/fr/the-lausanner/auteur/',
+        en: 'https://www.lausanne-tourisme.ch/en/the-lausanner/author/',
+        de: 'https://www.lausanne-tourisme.ch/de/the-lausanner/autor/'
+      }[locale] + lausanner.seo?.slug
+    );
   };
 
   const handleLausannerClick = ({
     favorite,
     lausanner,
-    isMarkerClick,
+    isMarkerClick
   }: {
     favorite: Favorite;
     lausanner?: Lausanner;
-    isMarkerClick?: boolean,
+    isMarkerClick?: boolean;
   }) => {
     const poi = favorite.pois?.at(0);
     favoriteId = favorite.id;
@@ -85,19 +98,19 @@
     aside.lausanner = lausanner ?? favorite.lausanner;
     aside.show = true;
 
-    const geolocation = poi?.geolocations?.at(0);
-		const marker = getMarker(favorite.id);
+    // const geolocation = poi?.geolocations?.at(0);
+    // const marker = getMarker(favorite.id);
 
-		if (!isMarkerClick) marker?.marker?._element?.click();
+    // if (!isMarkerClick) marker?.marker?._element?.click();
 
-		map?.flyTo({
-			center: [
-				Number(geolocation?.longitude ?? ''),
-				Number(geolocation?.latitude ?? ''),
-			],
-			zoom: 15,
-			essential: true
-		});
+    // map?.flyTo({
+    // 	center: [
+    // 		Number(geolocation?.longitude ?? ''),
+    // 		Number(geolocation?.latitude ?? ''),
+    // 	],
+    // 	zoom: 15,
+    // 	essential: true
+    // });
   };
 
   const closeAside = () => {
@@ -108,101 +121,14 @@
 
     favoriteId = undefined;
 
-    const boundsFromMarkers = getBoundsFromMarkers(markers);
+    // const boundsFromMarkers = getBoundsFromMarkers(markers);
 
-    map?.fitBounds(boundsFromMarkers, {
-    	padding: 20
-    });
-  };
-
-  const getLausannerUrl = (lausanner: Lausanner | undefined): string => {
-    if (!lausanner) return '#';
-
-    return (
-      {
-        fr: 'https://www.lausanne-tourisme.ch/fr/the-lausanner/auteur/',
-        en: 'https://www.lausanne-tourisme.ch/en/the-lausanner/author/',
-        de: 'https://www.lausanne-tourisme.ch/de/the-lausanner/autor/'
-      }[($locale as Locale) ?? defaultLocale] + lausanner.seo?.slug
-    );
-  };
-
-  const getMarker = (favoriteId: undefined | number) => markers.find((m) => m.key === favoriteId);
-
-  const markerClick = (marker: Marker) => {
-		getMarker(favoriteId)?.popup?.remove();
-		const favorite: Favorite = favorites.find((favorite: Favorite) => favorite.id === marker.key)!;
-
-    handleLausannerClick({favorite, lausanner: favorite.lausanner, isMarkerClick: true})
-  }
-
-  const newPopup = ({
-    id,
-    background,
-    img,
-    title
-  }: {
-    id: number;
-    background: string;
-    img: string;
-    title: string;
-  }): maplibregl.Popup => {
-    const popup = new Popup({ className: 'popup', closeOnClick: true }).setHTML(`
-				<div data-key="${id}" class="${background} text-center max-w-[320px] rounded-l-[3px] pl-1">
-						<h3 class="p-1 text-white">${title}</h3>
-				</div>
-			`);
-
-    popup
-      .on('open', () => {
-        popup._closeButton?.addEventListener('click', closeAside);
-      })
-      .on('close', () => {
-        aside.show = false;
-        getMarker(favoriteId)?.popup?.remove();
-      });
-
-    return popup;
-  };
-
-  const mapClick = (event: MouseEvent) => {
-    let target: HTMLElement | null | undefined = event.target as HTMLElement | null | undefined;
-
-    if (!target) return;
-
-    if (['PATH', 'SVG'].includes(target.nodeName)) {
-      while (target && target.classList.contains('maplibregl-marker')) {
-        target = target.parentElement as HTMLElement | null | undefined;
-      }
-
-      target?.click();
-    }
+    // map?.fitBounds(boundsFromMarkers, {
+    // 	padding: 20
+    // });
   };
 
   onMount(() => {
-    if (!favorites.length) {
-      console.error('An expected error occurred while getting favorites, please refresh.');
-    }
-
-    map = new Map({
-      container: mapContainer,
-      style: PUBLIC_MAPTILER_URL,
-      center: [initialState.lng, initialState.lat],
-      zoom: initialState.zoom
-    });
-
-    map.addControl(new NavigationControl(), 'top-right');
-
-    map.on('wheel', (event) => {
-      if (event.originalEvent.ctrlKey) {
-        // Check if CTRL key is pressed
-        event.originalEvent.preventDefault(); // Prevent chrome/firefox default behavior
-        if (!map?.scrollZoom._enabled) map?.scrollZoom.enable(); // Enable zoom only if it's disabled
-      } else {
-        if (map?.scrollZoom._enabled) map?.scrollZoom.disable(); // Disable zoom only if it's enabled
-      }
-    });
-
     favorites.forEach((favorite: Favorite) => {
       const { pois, lausanner } = favorite;
 
@@ -210,37 +136,23 @@
         const { geolocations } = poi;
         geolocations?.forEach((geolocation: Geolocation) => {
           markers.push({
-            key: favorite.id as number,
+            poiName: poi.name as string,
+            favorite,
+            lausanner,
             coordinates: {
               lat: parseFloat(geolocation.latitude),
               lng: parseFloat(geolocation.longitude)
-            },
-            popup: newPopup({
-              id: favorite.id as number,
-              background: themeColor ?? '',
-              img: Cloudinary.make(
-                `${PUBLIC_CLOUDINARY_UPLOAD_PRESET}/${lausanner?.medias?.at(0)?.cloudinary_id}`
-              ).url({ w: 60 }),
-              title: poi.name as string
-            }),
-            backgroundColor: themeColor
+            }
           });
         });
       });
     });
 
-    if (markers.length) {
-      const boundsFromMarkers = getBoundsFromMarkers(markers);
-      map.fitBounds(boundsFromMarkers, {
-        padding: 20
-      });
-    }
-
-    return () => map?.remove();
+    document.documentElement.style.setProperty('--popup-color', getTailwindColor(themeColor ?? ''));
   });
 </script>
 
-<div class="relative flex h-[550px] flex-col-reverse md:flex-row 2xl:h-[768px]">
+<div class="relative flex h-[550px] md:flex-row 2xl:h-[768px]">
   {#if favorites.length}
     <section class="map-tips z-0 h-full w-full overflow-y-hidden bg-gray-100 md:w-2/5">
       <div class={twMerge('relative h-full overflow-y-scroll p-4', aside.show ? 'hidden' : '')}>
@@ -281,18 +193,16 @@
                 ? '/images/pages/themes/user_not_found.png'
                 : (aside.lausanner?.medias?.at(0)?.cloudinary_id ??
                   '/images/pages/themes/user_not_found.png')}
-              transform={{
-                g: 'north',
-                c: 'auto',
-                w: 48,
-                h: 48
-              }}
+              transform={{ g: 'north', c: 'auto', w: 48, h: 48 }}
             />
             <div class="flex w-full items-center justify-between">
               <Paragraph class="ml-2 inline-flex w-1/2 text-sm font-bold md:w-2/3 md:text-base">
                 <Link
                   withIcon={true}
-                  href={getLausannerUrl(aside.lausanner)}
+                  href={getLausannerUrl({
+                    lausanner: aside.lausanner,
+                    locale: ($locale as Locale) ?? defaultLocale
+                  })}
                   class="text-left xl:text-center"
                 >
                   {aside.lausanner?.name}
@@ -315,18 +225,92 @@
       {/if}
     </section>
   {/if}
-  <!--display map without lausanners if problem on loading-->
-  <section
-    class="map-wrap relative h-full w-full {favorites.length ? 'md:w-3/5' : ''} my-2 md:my-0 overflow-hidden"
+  <MapLibre
+    class="h-full w-full"
+    style={PUBLIC_MAPTILER_URL}
+    zoom={initialState.zoom}
+    center={initialState}
+    locale={$locale}
+    pitchWithRotate={false}
+    cooperativeGestures={true}
   >
-    <!-- svelte-ignore a11y_click_events_have_key_events -->
-    <!-- svelte-ignore a11y_no_static_element_interactions -->
-    <div id="map" class="h-full w-full" bind:this={mapContainer} onclick={mapClick}></div>
-  </section>
+    <NavigationControl position={'top-right'} />
+    {#each markers as marker}
+      <Marker lnglat={marker.coordinates}>
+        {#snippet content()}
+          <MapPin class="stroke-brand-500 h-6 w-6 scale-90 text-transparent" strokeWidth={3} />
+        {/snippet}
+        <Popup
+          open={favoriteId === marker.favorite.id}
+          onopen={() => {
+            setTimeout(() => handleLausannerClick({favorite: marker.favorite, isMarkerClick:true}), 50);
+          }}
+          onclose={closeAside}
+          offset={{
+            top: [0, 12],
+            bottom: [0, -12],
+            left: [12, 0],
+            right: [-12, 0],
+            center: [0, 0],
+            'top-left': [0, 12],
+            'top-right': [0, 12],
+            'bottom-left': [0, -12],
+            'bottom-right': [0, -12]
+          }}
+        >
+          <span class="text-white text-base p-2">{marker.poiName}</span>
+        </Popup>
+      </Marker>
+    {/each}
+  </MapLibre>
 </div>
 
-{#if map}
-  {#each markers as marker }
-    <MarkerComponent {marker} {map} onclick={markerClick}/>
-  {/each}
-{/if}
+<style lang="postcss">
+  :global(.maplibregl-cooperative-gesture-screen) {
+    z-index: 1 !important;
+  }
+
+  :global(.maplibregl-popup-anchor-bottom .maplibregl-popup-tip) {
+    border-top-color: var(--popup-color) !important;
+  }
+  :global(.maplibregl-popup-anchor-left .maplibregl-popup-tip) {
+    border-right-color: var(--popup-color) !important;
+  }
+  :global(.maplibregl-popup-anchor-right .maplibregl-popup-tip) {
+    border-left-color: var(--popup-color) !important;
+  }
+  :global(.maplibregl-popup-anchor-top .maplibregl-popup-tip) {
+    border-bottom-color: var(--popup-color) !important;
+  }
+
+  :global(.maplibregl-popup-anchor-top-right .maplibregl-popup-tip) {
+    border-bottom-color: var(--popup-color) !important;
+  }
+  :global(.maplibregl-popup-anchor-top-left .maplibregl-popup-tip) {
+    border-bottom-color: var(--popup-color) !important;
+  }
+  :global(.maplibregl-popup-anchor-bottom-right .maplibregl-popup-tip) {
+    border-top-color: var(--popup-color) !important;
+  }
+  :global(.maplibregl-popup-anchor-bottom-left .maplibregl-popup-tip) {
+    border-top-color: var(--popup-color) !important;
+  }
+
+  :global(.maplibregl-popup-content) {
+    display: flex !important;
+    background-color: var(--popup-color) !important;
+    padding: 0 !important;
+    font-family: 'Euclid Flex', sans-serif !important;
+    font-weight: bold;
+  }
+
+  :global(.maplibregl-popup-content div) {
+    max-width: 320px !important;
+    text-align: center !important;
+  }
+
+  :global(.maplibregl-popup-content button) {
+    position: unset;
+    width: 20px;
+  }
+</style>
