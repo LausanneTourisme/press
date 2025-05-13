8fe9f841850470a276bda670fa9604dd82a49619
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-05-13 16:36:03 +0200

    Create Types for content's Post

---
+++ src/lib/types/releaseContents.ts
@@ -0,0 +1,47 @@
+import type { Media, Translatable, Video as NovaVideo } from "./nova"
+
+export type Hero = {
+    type: "hero",
+    tag: `h${1 | 2 | 3 | 4 | 5 | 6}`,
+    value: Translatable,
+    lead: Translatable,
+    color: "white" | "black",
+    image: Media<Translatable>,
+}
+
+export type Heading = {
+    type: "heading",
+    tag: `h${1 | 2 | 3 | 4 | 5 | 6}`,
+    value: Translatable,
+}
+
+export type Paragraphe = {
+    type: "paragraph",
+    value: Translatable,
+}
+
+export type Gallery = {
+    type: "gallery",
+    value: Media<Translatable>[],
+}
+
+export type Image = {
+    type: "image",
+    size: "parallax" | "small" | "medium" | "large",
+    focus: "auto" | "face",
+    value: Media<Translatable>,
+}
+
+export type Instagram = {
+    type: "instagram",
+    value: string,
+}
+
+export type Youtube = {
+    type: "youtube",
+    value: string,
+}
+export type Video = {
+    type: "video",
+    value: NovaVideo<Translatable>
+}
