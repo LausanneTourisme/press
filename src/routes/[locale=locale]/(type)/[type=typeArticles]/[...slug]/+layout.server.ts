37ef42006bfaff05bed30dfec37983414c2b463b
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-05-19 10:04:29 +0200

    fix Layout

--- src/routes/[locale=locale]/(type)/[type=typeArticles]/[...slug]/+layout.server.ts
+++ src/routes/[locale=locale]/(type)/[type=typeArticles]/[...slug]/+layout.server.ts
@@ -4,7 +4,7 @@ import { Cloudinary } from '$lib/cloudinary';
 import { isOfflineMode } from '$lib/helpers';
 import { getArticle } from '$lib/helpers/requests.server';
 import { server } from '$lib/mocks/handler';
-import { loadTranslations, supportedLocales } from '$lib/translations';
+import { loadTranslations, supportedLocales, type Locale } from '$lib/translations';
 import type { SeoHeader, Translatable } from '$types';
 import { error } from '@sveltejs/kit';
 
@@ -27,12 +27,12 @@ export const load = async ({ params, parent, url, ...rest }) => {
 
     const seo: SeoHeader = {
         canonical: `${url.origin}${url.pathname}`,
-        title: (article.name as undefined | Translatable)?.[locale] ?? translations[locale][`page.title`],
-        description: (article.lead as undefined | Translatable)?.[locale] ?? translations[locale][`page.meta-description`],
+        title: article.name?.[locale as Locale] ?? translations[locale][`page.title`],
+        description: article.lead?.[locale as Locale] ?? translations[locale][`page.meta-description`],
         image: Cloudinary.make(article.medias?.at(0)?.cloudinary_id ?? '').url({ h: 720, w: 1280 }),
         alternate: supportedLocales.map(locale => ({
             hreflang: locale,
-            href: `/${locale}/${translations[locale][`route.${RouteTypes.Articles}.slug`]}/${(article.seo?.slug as undefined | Translatable)?.[locale]}`
+            href: `/${locale}/${translations[locale][`route.${RouteTypes.Articles}.slug`]}/${article.seo?.slug?.[locale]}`
         })),
     }
 
