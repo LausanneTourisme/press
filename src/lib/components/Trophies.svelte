f39b7fe8ab32533cc828c8ad263180bf733f87a4
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-06-12 10:18:50 +0200

    Fix Trophies to use new Swiper

--- src/lib/components/Trophies.svelte
+++ src/lib/components/Trophies.svelte
@@ -5,45 +5,45 @@
   import Container from './Container.svelte';
   import Heading from './Heading.svelte';
   import Paragraph from './Paragraph.svelte';
-  import Swiper from './Swiper.svelte';
+  import {Swiper, Slide} from './swiper';
 
   export type Trophy = {
-      name: string;
-      content: string;
-      image: string;
-      link: string;
-    }
+    name: string;
+    content: string;
+    image: string;
+    link: string;
+  };
   type Props = {
     values: Trophy[];
+    isMobile?: boolean;
     class?: string;
   };
 
-  const { class: additionalClass, values }: Props = $props();
+  const { class: additionalClass, isMobile = false, values }: Props = $props();
 
   const style = twMerge('md:mx-auto gap-8 p-4', 'flex', additionalClass);
 </script>
 
 <Container width="large">
-  <Swiper options={{ slidesToScroll: 1 }} containerClass={style}>
-    {#each values as article, k}
-      <Clickable overflow={true} href={article.link}>
-        <Card
-          src={article.image}
-          alt={article.name}
-          background="bg-glacier-300"
-          class="carousel-item"
-          nofx={true}
-        >
-          <Heading tag="h3" class="text-white text-shadow:_0_0_20px_var(--tw-shadow-color)] shadow-gray-950">
-            {article.name}
-          </Heading>
-          <Paragraph
-            class="text-shadow:_0_0_2px_var(--tw-shadow-color)] text-white shadow-gray-950"
-          >
-            {article.content}
-          </Paragraph>
-        </Card>
-      </Clickable>
-    {/each}
+  <Swiper>
+      {#each values as article, k}
+        <Slide>
+          <Clickable href={article.link}>
+            <Card src={article.image} alt={article.name} background="bg-glacier-300" nofx={true}>
+              <Heading
+                tag="h3"
+                class="text-shadow:_0_0_20px_var(--tw-shadow-color)] text-white shadow-gray-950"
+              >
+                {article.name}
+              </Heading>
+              <Paragraph
+                class="text-shadow:_0_0_2px_var(--tw-shadow-color)] line-clamp-none text-white shadow-gray-950"
+              >
+                {article.content}
+              </Paragraph>
+            </Card>
+          </Clickable>
+        </Slide>
+      {/each}
   </Swiper>
 </Container>
