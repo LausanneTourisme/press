e15ea611c1700aac407a92b2046d7f4c4b98259b
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-06-12 10:18:30 +0200

    refactor Card component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { twMerge } from 'tailwind-merge';
   import { type Snippet } from 'svelte';
+  import { twMerge } from 'tailwind-merge';
   import Image from './Media/Image.svelte';
 
   type Props = {
@@ -26,26 +26,26 @@
   }: Props = $props();
 
   const cardStyle = twMerge(
-    'card rounded-none',
-    nofx ? 'nofx' : '',
-    'min-w-[315px] w-[315px] md:w-[375px] h-[360px] md:h-[460px] relative md:ml-0',
-    'group transition-all',
-    'bg-gradient-to-b from-transparent to-zinc-950 hover:to-transparent',
+    'relative',
+    'min-w-[220px] xs:min-w-[300px] w-full',
+    'h-[460px]',
+    'md:ml-0 group transition-all',
     negative ? 'text-zinc-950' : 'text-white',
     background,
     additionalClass
   );
   const paragraphStyle = twMerge(
-    'body flex flex-col justify-end relative z-0 p-6 pb-8 w-full h-full drop-shadow-md',
-    border,
-    negative ? 'text-zinc-950' : 'text-white',
+    'relative drop-shadow-md',
     '[text-shadow:_0_0_7px_var(--tw-shadow-color)] shadow-zinc-950/80',
+    'flex flex-col justify-end',
+    'p-6 pb-8',
+    'w-full h-full'
   );
 </script>
 
 <div class={cardStyle}>
   <div class="image pointer-events-none absolute top-0 left-0 h-full w-full">
-    <Image {src} {alt} class="opacity-100 transition-all group-hover:opacity-100" />
+    <Image {src} {alt} class="" />
   </div>
   <div
     class="to-shakespeare-950/80 pointer-events-none absolute top-0 left-0 h-full w-full bg-gradient-to-b from-transparent from-0% via-transparent via-40% to-100% transition-all group-hover:opacity-70"
