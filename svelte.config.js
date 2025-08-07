0055039c425425a6a8e4ac1cd85c587f583038a5
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-08-07 23:01:16 +0200

    update + remove pre-render in config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import path from 'path';
 import { readFileSync } from 'node:fs';
 
@@ -7,11 +7,12 @@ const pkg = JSON.parse(json);
 json = readFileSync(path.resolve('./src/lib/translations/fr/lang.json'), 'utf8');
 const supportedLocales = Object.keys(JSON.parse(json));
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     version: {
-      name: pkg.version
+      name: `Presskit v${pkg.version}`
     },
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -22,10 +23,6 @@ const config = {
       $types: path.resolve('./src/lib/types'),
     },
 
-    prerender: {
-      // NOTE: You can modify your exported error pages here.
-      entries: supportedLocales.flatMap((locale) => [`/${locale}`, `/${locale}/401`, `/${locale}/403`, `/${locale}/404`, `/${locale}/500`]),
-    }
   }
 };
 
