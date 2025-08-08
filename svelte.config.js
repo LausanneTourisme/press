3f69e81169c2d4567fce9e42d95f798409f012e6
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-08-08 12:05:02 +0200

    fix svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,19 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 import { readFileSync } from 'node:fs';
 
-let json = readFileSync(path.resolve('./package.json'), 'utf8');
+const json = readFileSync(path.resolve('./package.json'), 'utf8');
 const pkg = JSON.parse(json);
-json = readFileSync(path.resolve('./src/lib/translations/fr/lang.json'), 'utf8');
-const supportedLocales = Object.keys(JSON.parse(json));
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
   kit: {
     version: {
       name: `Presskit v${pkg.version}`
     },
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
     alias: {
       $enums: path.resolve('./src/lib/enums'),
