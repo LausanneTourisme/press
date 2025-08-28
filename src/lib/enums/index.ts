61d97fe4e73460bed1d43d2920a98a6d13f15767
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-08-28 15:11:33 +0200

    create new enums : MediaType, TravelReduction, Gender

--- src/lib/enums/index.ts
+++ src/lib/enums/index.ts
@@ -1,6 +1,9 @@
 export type RouteType = typeof RouteTypes[keyof typeof RouteTypes]
 export type Theme = typeof Themes[keyof typeof Themes]
 export type Form = typeof Forms[keyof typeof Forms]
+export type MediaType = typeof MediaTypes[keyof typeof MediaTypes]
+export type TravelReduction = typeof TravelReductions[keyof typeof TravelReductions]
+export type Gender = typeof Genders[keyof typeof Genders]
 
 //ThemeKeys available under 😉 needs reference of Themes
 
@@ -36,6 +39,25 @@ export const Forms = {
   ContentCreator: 'content-creator',
 } as const;
 
+export const MediaTypes = {
+  Print: 'print',
+  Online: 'online',
+  Tv: 'tv',
+  Radio: 'radio',
+} as const;
+
+export const TravelReductions = {
+  PointToPointTravelcard: 'point-to-point-travelcard',
+  HalfFare: 'half-fare',
+  SwissGATravelCard: 'swiss-ga-travelcard',
+} as const;
+
+export const Genders = {
+  Mrs: 'mrs',
+  Mr: 'mr',
+  They: 'they',
+} as const;
+
 // generic helper
 function invert<T extends Record<string, string>>(obj: T) {
   return Object.fromEntries(
@@ -43,6 +65,13 @@ function invert<T extends Record<string, string>>(obj: T) {
   ) as { [K in keyof T as T[K]]: K };
 }
 
+export function getValues<T extends Record<string, any>>(obj: T) {
+  return Object.values(obj) as [(typeof obj)[keyof T]]
+}
+
 // usage
 export const ThemeKeys = invert(Themes);
 export const FormsKeys = invert(Forms);
+export const MediaTypesKeys = invert(MediaTypes);
+export const TravelReductionsKeys = invert(TravelReductions);
+export const GendersKeys = invert(Genders);
