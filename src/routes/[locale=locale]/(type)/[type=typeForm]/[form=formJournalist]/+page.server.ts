924873e4265c7cb5918b8f020df2e091ca3a239b
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-09-01 08:37:44 +0200

    first poc of form iteration

--- src/routes/[locale=locale]/(type)/[type=typeForm]/[form=formJournalist]/+page.server.ts
+++ src/routes/[locale=locale]/(type)/[type=typeForm]/[form=formJournalist]/+page.server.ts
@@ -4,14 +4,14 @@ import countries from 'i18n-iso-countries';
 import de from "i18n-iso-countries/langs/de.json";
 import en from "i18n-iso-countries/langs/en.json";
 import fr from "i18n-iso-countries/langs/fr.json";
-import { superValidate, type Infer } from 'sveltekit-superforms';
+import { message, superValidate, type Infer } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import type { EntryGenerator } from "./$types";
 import { schemaStep1, schemaStep2, schemaStep3, schemaStep4 } from "./schema";
 
 type Message = { step: number; message?: string };
 const countriesByLocale: Record<string, any> = { en, fr, de };
-const steps = [zod(schemaStep1), zod(schemaStep2), zod(schemaStep3), zod(schemaStep4),] as const
+const steps = [zod(schemaStep1), zod(schemaStep2), zod(schemaStep3), zod(schemaStep4)]
 const lastStep = zod(schemaStep4);
 
 export const load = async ({ parent }) => {
@@ -25,6 +25,18 @@ export const load = async ({ parent }) => {
     return {
         countries: Object.values(countries.getNames(locale, { select: "official" })).sort(),
         form,
+    }
+}
+
+
+export const actions = {
+    default: async ({ request }) => {
+        const formData = await request.formData()
+        console.log({ formData })
+
+        const step = +(formData.get('step') ?? 0);
+        const form = await superValidate(formData, steps[step]);
+        return message(form, { oldStep: step, text: 'Form posted successfully!', step: (step + 1) % 4 });
     }
 }
 
--- src/routes/[locale=locale]/(type)/[type=typeForm]/[form=formJournalist]/+page.svelte
+++ src/routes/[locale=locale]/(type)/[type=typeForm]/[form=formJournalist]/+page.svelte
@@ -1,11 +1,110 @@
 <script lang="ts">
   import { page } from '$app/state';
+  import { superForm } from 'sveltekit-superforms';
   import type { PageData } from './$types';
+  import { t } from '$lib/translations';
+  import { Forms, RouteTypes } from '$enums';
+  import type { MediaProfileJournalist } from '$types';
+  // TODO SEO
+  const countries = $derived(Object.values((page.data as PageData).countries));
+  const { form, formId, errors, message, capture, restore } = $derived.by(() =>
+    superForm((page.data as PageData).form, {
+      dataType: 'json',
+      onSubmit: async ({ cancel }) => {}
+    })
+  );
+  const step = $derived($message?.step ?? 0);
 
-  let countries = $derived(Object.values((page.data as PageData).countries));
+  $effect(() => {
+    console.log({ errors: $errors, message: $message });
+  });
+
+  const mediaProfile: MediaProfileJournalist = {
+    mediaName: undefined,
+    thematic: undefined,
+    audienceProfile: undefined,
+    mediaType: [],
+    printMediaStatistics: {
+      copies: 0,
+      readers: 0,
+      broadcastLocation: undefined
+    },
+    radioAndTVMediaStatistics: {
+      emissionName: undefined,
+      viewers: 0
+    },
+    onlineMediaStatistics: {
+      website: undefined,
+      monthlyUniqueVisitors: 0,
+      montlhyPageViews: 0
+    },
+    mediaCoveragePrint: {
+      totalPages: 0,
+      articleLength: 0,
+      publishDate: undefined
+    },
+    mediaCoverageOnline: {
+      articleLength: 0,
+      articleThematic: 0,
+      publishDate: undefined
+    },
+    mediaCoverageTvOrRadio: {
+      articleThematic: undefined,
+      publishDate: undefined
+    },
+    travelInformation: {
+      departurePoint: {
+        city: undefined,
+        country: undefined,
+        outwardJourney: undefined
+      },
+      returnJourney: undefined,
+      anyReduction: [],
+      lastVisit: undefined
+    },
+    personalInformation: {
+      title: undefined,
+      firstName: undefined,
+      lastName: undefined,
+      birthday: undefined,
+      phoneNumber: undefined,
+      email: undefined,
+      address: {
+        address: undefined,
+        city: undefined,
+        postalcode: undefined,
+        country: undefined
+      },
+      freelance: undefined,
+      spokenLanguages: undefined,
+      medicalAndPhysicalCondition: undefined,
+      passport: {
+        number: undefined,
+        validity: undefined
+      },
+      emergencyContacts: [],
+    },
+    travelInsuranceCoveringSwitzerland: undefined,
+    remarks: undefined,
+    readTermsOfAcceptance: undefined,
+    newsletter: undefined
+  };
 </script>
 
 <h2>Media information</h2>
-{#each countries as country}
-    {country} <br>
-{/each}
+<!-- {#each countries as country}
+  {country} <br />
+{/each} -->
+
+<form method="POST">
+  <input type="hidden" name="step" value={step} />
+  <input type="hidden" name="__superform_id" bind:value={$formId} />
+  Step{step}<br />
+
+  <section class="step1">
+    <label for="media-name">{$t(`${RouteTypes.Form}.${Forms.Journalist}.media-name`)}</label>
+    <input type="text" id="media-name" />
+  </section>
+
+  <button>Submit</button>
+</form>
