e43b27f17a2728c6ea1038076dffee6330291a31
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-09-08 08:52:12 +0200

    step 0 complete !

--- src/lib/helpers/zod.ts
+++ src/lib/helpers/zod.ts
@@ -3,5 +3,5 @@ import { z } from 'zod';
 export const emptyStringToUndefined = (s: unknown) =>
     typeof s === 'string' && s.trim() === '' ? undefined : s;
 
-export const zodRequiredString = (args?: {message?: string}) => z.preprocess(emptyStringToUndefined, z.string().min(2), { message: args?.message });
-export const zodOptionalString = (args?: {message?: string}) => z.preprocess(emptyStringToUndefined, z.string().nullish(), { message: args?.message });
+export const zodRequiredString = (args?: {message?: string, min?: number}) => z.preprocess(emptyStringToUndefined, z.string().min(args?.min ?? 2), { message: args?.message });
+export const zodOptionalString = (args?: {message?: string, min?: number}) => z.preprocess(emptyStringToUndefined, z.string().min(args?.min ?? 0).nullish(), { message: args?.message });
