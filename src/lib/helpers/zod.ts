95211c0bd1db9b050af96229f7de950add9ab824
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-09-10 13:51:26 +0200

    update zod + fix schemas

--- src/lib/helpers/zod.ts
+++ src/lib/helpers/zod.ts
@@ -3,5 +3,5 @@ import { z } from 'zod';
 export const emptyStringToUndefined = (s: unknown) =>
     typeof s === 'string' && s.trim() === '' ? undefined : s;
 
-export const zodRequiredString = (args?: {message?: string, min?: number}) => z.preprocess(emptyStringToUndefined, z.string().min(args?.min ?? 2).nonempty(), { message: args?.message });
-export const zodOptionalString = (args?: {message?: string, min?: number}) => z.preprocess(emptyStringToUndefined, z.string().min(args?.min ?? 0).nullish(), { message: args?.message });
+export const zodRequiredString = (args?: { error?: string, min?: number }) => z.string().min(args?.min ?? 2, { error: args?.error }).nonempty({ error: args?.error })
+export const zodOptionalString = (args?: { error?: string, min?: number }) => z.string().min(args?.min ?? 0, {error: args?.error}).optional()
