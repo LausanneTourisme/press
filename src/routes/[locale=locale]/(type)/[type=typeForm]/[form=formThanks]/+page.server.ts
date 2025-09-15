cb5406162ad1e89bea73a8de7744c3d8338b55b2
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-09-15 14:44:28 +0200

    create thanks page

---
+++ src/routes/[locale=locale]/(type)/[type=typeForm]/[form=formThanks]/+page.server.ts
@@ -0,0 +1,15 @@
+import { Forms, RouteTypes } from "$enums";
+import { supportedLocales, translations } from "$lib/translations";
+import type { EntryGenerator } from "../$types";
+
+export const entries: EntryGenerator = () => {
+    const t = translations.get();
+
+    return supportedLocales.flatMap(locale => {
+        return {
+            locale,
+            type: t[locale][`route.${RouteTypes.Form}.slug`],
+            form: t[locale][`route.${RouteTypes.Form}.${Forms.Thanks}.slug`]
+        };
+    });
+};
