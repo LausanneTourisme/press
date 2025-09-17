a9e32cff82cc2adf177ddc7edf23e0f422eea473
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-09-17 11:55:02 +0200

    update types to add "forms"

---
+++ src/lib/types/forms.ts
@@ -0,0 +1,74 @@
+import type { MediaType, SocialNetwork, Title, TravelReduction } from "$enums"
+
+
+export type MediaProfileJournalist = {
+    mediaName?: string,
+    thematic?: string,
+    audienceProfile?: string,
+    mediaTypes: MediaType[],
+    printMediaStatistics: {
+        copies?: number,
+        readers?: number,
+        broadcastLocation?: string
+    },
+    radioAndTVMediaStatistics: {
+        emissionName?: string,
+        viewers?: number
+    },
+    onlineMediaStatistics: {
+        website?: string,
+        monthlyUniqueVisitors?: number,
+        montlhyPageViews?: number
+    },
+    mediaCoveragePrint: {
+        totalPages?: number,
+        articleLength?: number,
+        publishDate?: string
+    },
+    mediaCoverageOnline: {
+        articleLength?: number,
+        articleThematic?: number,
+        publishDate?: string
+    },
+    mediaCoverageTvOrRadio: {
+        articleThematic?: string,
+        publishDate?: string
+    },
+    travelInformation: {
+        departurePoint: {
+            city?: string,
+            country?: string,
+            outwardJourney?: string
+        },
+        returnJourney?: string,
+        travelReductions: TravelReduction[],
+        lastVisit?: string
+    },
+    personalInformation: {
+        title?: Title,
+        firstName?: string,
+        lastName?: string,
+        birthday?: string,
+        phoneNumber?: string,
+        email?: string,
+        address: {
+            streetAddress?: string,
+            city?: string,
+            postalcode?: string,
+            country?: string
+        },
+        freelance?: boolean,
+        spokenLanguages?: string,
+        allergies?: string,
+        medicalAndPhysicalCondition?: string,
+        passport: {
+            number?: string,
+            validity?: string
+        },
+        emergencyContacts: { name?: string, phoneNumber?: string }[],
+    },
+    travelInsuranceCoveringSwitzerland?: boolean,
+    remarks?: string,
+    readTermsOfAcceptance?: boolean,
+    newsletter?: boolean
+}
