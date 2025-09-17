94d0ea9e69afab78ea1e1b9dcb0fb99eb0bdbb3c
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-09-17 11:55:23 +0200

    creates type MediaProfileContentCreator

--- src/lib/types/forms.ts
+++ src/lib/types/forms.ts
@@ -72,3 +72,78 @@ export type MediaProfileJournalist = {
     readTermsOfAcceptance?: boolean,
     newsletter?: boolean
 }
+
+export type MediaProfileContentCreator = {
+    socialMediaInformation: {
+        contentPositioning?: string,
+        targetAudience?: string,
+        onlinePresence: SocialNetwork[],
+        statistics: {
+            instagram: {
+                profileURL?: string,
+                subscriberStatisticsScreenshots: File[],
+                accountsThatRespondedScreenshots: File[],
+            },
+            tiktok: {
+                profileURL?: string,
+                subscriberStatisticsScreenshots: File[],
+            }
+            youtube: {
+                profileURL: string,
+                subscriberStatisticsScreenshots: File[],
+            },
+            blog: {
+                url?: string,
+                audienceProfile?: string,
+                performance?: {
+                    monthlyUniqueVisitors?: number,
+                    montlhyPageViews?: number
+                }
+            },
+        },
+        coverage: {
+            publicationAngle: string,
+            subjectsOfInterest: string,
+            publicationChannels: SocialNetwork[],
+            proposedMediaCoverage: string,
+            timingAndpublicationDates: string
+        },
+        travelInformation: {
+            departurePoint: {
+                city?: string,
+                country?: string,
+                outwardJourney?: string
+            },
+            returnJourney?: string,
+            travelReductions: TravelReduction[],
+            lastVisit?: string
+        },
+        personalInformation: {
+            title?: Title,
+            firstName?: string,
+            lastName?: string,
+            birthday?: string,
+            phoneNumber?: string,
+            email?: string,
+            address: {
+                streetAddress?: string,
+                city?: string,
+                postalcode?: string,
+                country?: string
+            },
+            freelance?: boolean,
+            spokenLanguages?: string,
+            allergies?: string,
+            medicalAndPhysicalCondition?: string,
+            passport: {
+                number?: string,
+                validity?: string
+            },
+            emergencyContacts: { name?: string, phoneNumber?: string }[],
+        },
+        travelInsuranceCoveringSwitzerland?: boolean,
+        remarks?: string,
+        readTermsOfAcceptance?: boolean,
+        newsletter?: boolean
+    }
+}
