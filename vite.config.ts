eb2b68921d98b42f8c8ff594ab02bee552a09350
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-10-23 10:34:28 +0200

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()]
+  plugins: [tailwindcss(), sveltekit()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+  resolve: process.env.VITEST
+		? {
+				conditions: ['browser']
+			}
+		: undefined,
+
 });
