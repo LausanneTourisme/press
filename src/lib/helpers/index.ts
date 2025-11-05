d37cef6dceef53ff058190b270ed6387ea0ac539
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-11-05 15:06:09 +0100

    fix index helper

--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -150,6 +150,9 @@ export const shuffle = <T>(array: T[]) => {
 };
 
 export const humanFileSize = (size: number) => {
-  const i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
-  return +(size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['o', 'ko', 'Mo', 'Go', 'To'][i];
+  if (size === 0) return '0 o';
+
+  const i = Math.floor(Math.log(size) / Math.log(1024));
+  const index = Math.min(i, 4); // Cap at index 4 (To)
+  return +(size / Math.pow(1024, index)).toFixed(2) + ' ' + ['o', 'ko', 'Mo', 'Go', 'To'][index];
 };
