91902ce7c7504a419299e20629c852100b04b064
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2026-03-24 16:44:48 +0100

    allow custom build output

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
     version: {
       name: `Presskit v${pkg.version}`
     },
-    adapter: process.env.ENVIRONMENT === 'node' ? node() : vercel(),
+    adapter: process.env.ENVIRONMENT === 'node' ? node({ out: process.env.BUILD_DIR || 'build' }) : vercel(),
     alias: {
       $enums: path.resolve('./src/lib/enums'),
       $types: path.resolve('./src/lib/types')
