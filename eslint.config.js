dcf379671f2df0c83745efe9334f4a5422d3bf48
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2026-03-24 12:21:25 +0100

    update prettier

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,7 @@ const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
   includeIgnoreFile(gitignorePath),
+  { ignores: ['.app/**'] },
   js.configs.recommended,
   ...ts.configs.recommended,
   ...svelte.configs['flat/recommended'],
