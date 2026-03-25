35553c8198154460ccffb8d027e9253fa64d69ed
diogo-vf <diogo.vieira-ferreira@proton.me>
2026-03-25 10:54:19 +0100

    create vitest.config.js

---
+++ vitest.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit()],
+  test: {
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  resolve: {
+    conditions: ['browser']
+  }
+});
