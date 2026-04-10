1ba380f8aeea8d8a5cff43a59b32d798dfd52aff
diogo-vf <diogo.vieira-ferreira@proton.me>
2026-04-11 00:18:06 +0200

    add mail types

---
+++ src/lib/types/mail.types.ts
@@ -0,0 +1,20 @@
+export type MailAttachment = {
+  name: string; // filename / cid for images
+  type: string; // MIME type
+  content: string; // base64 encoded
+};
+
+export type InternMail = {
+  from_name: string;
+  subject: string;
+  html: string;
+  attachments?: MailAttachment[];
+  images?: MailAttachment[];
+};
+
+export type ExternalMail = {
+  from_name?: string;
+  subject: string;
+  html: string;
+  to: { email: string }[];
+};
