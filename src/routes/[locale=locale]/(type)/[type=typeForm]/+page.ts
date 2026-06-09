4a17e1432c871d9cff021bc68c95f0cd4112d7e4
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2026-06-09 12:16:09 +0200

    fix prerender

--- src/routes/[locale=locale]/(type)/[type=typeForm]/+page.ts
+++ src/routes/[locale=locale]/(type)/[type=typeForm]/+page.ts
@@ -1,7 +1,14 @@
-export const prerender = true;
+import { error } from '@sveltejs/kit';
+
+export const prerender = false;
 
 export const config = {
   isr: {
     expiration: 31536000
   }
+};
+
+export const load = ({ setHeaders}) => {
+  setHeaders({ 'cache-control': 'public, s-maxage=31536000, immutable' });
+  throw error(404);
 };
