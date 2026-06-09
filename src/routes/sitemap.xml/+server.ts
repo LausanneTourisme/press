c0eba422a0f83011e8e2043f87522bca696cee8f
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2026-06-09 11:50:24 +0200

    add version to xml stylesheet to break cache

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,3 +1,4 @@
+import { version } from '$app/environment';
 import type { RequestHandler } from '@sveltejs/kit';
 import { generateUrlSets, getInternalLinks } from '$lib/helpers/sitemap';
 
@@ -6,7 +7,7 @@ export const GET: RequestHandler = async ({ url, fetch }) => {
   const urlSets = generateUrlSets({ entries, urlOrigin: url.origin });
 
   return new Response(
-    `<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/css" href="/sitemap.css"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n${urlSets.join('\n')}\n</urlset>`,
+    `<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/css" href="/sitemap.css?v=${encodeURIComponent(version)}"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n${urlSets.join('\n')}\n</urlset>`,
     {
       headers: {
         'Content-Type': 'application/xml'
