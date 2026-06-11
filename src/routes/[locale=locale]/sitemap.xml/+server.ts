72897d4a7cb5fcce01059602203516b6be165cca
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2026-06-11 10:54:26 +0200

    maybe cloudflare will not remove style ?

--- src/routes/[locale=locale]/sitemap.xml/+server.ts
+++ src/routes/[locale=locale]/sitemap.xml/+server.ts
@@ -10,7 +10,8 @@ export const GET: RequestHandler = async ({ url, fetch, params }) => {
     `<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/css" href="/sitemap.css?v=${encodeURIComponent(version)}"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n${urlSets.join('\n')}\n</urlset>`,
     {
       headers: {
-        'Content-Type': 'application/xml'
+        'Content-Type': 'application/xml',
+        'Cache-Control': 'no-transform'
       }
     }
   );
